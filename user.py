cc5710830e989ad1162f0690ca20afb73966816f
annngure <annwanjikungure@gmail.com>
2022-01-24 09:00:41 +0300

    add user codes

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    pass
+    class that generate new instances of User
+    user_list=[] #list to be stored here
+
+    def _init_ (self,username,password):
+        pass
+        save user credentials into user_list for login
+        self.username=username
+        self.password=password
+
+        def delete_user(self):
+            pass
+            delete a user account
+            pass 
+            User.user_list.remove(self)
+            
