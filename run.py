d33941ac77c919c079bac4d1f9a40a6cbff96376
annngure <annwanjikungure@gmail.com>
2022-01-25 14:18:05 +0300

    run.py it works!

---
+++ run.py
@@ -0,0 +1,30 @@
+# import radom
+# import pyperclip
+# from user import User
+# from credentials import Credentials
+
+usernames=[]
+passwords=[]
+email=[]
+
+def register():
+  usernames.append(input("Enter your username"))
+  passwords.append(input("Enter your password"))
+  email.append(input("Enter your email"))
+
+def login():
+    username = input("Enter your username")
+    password= input("Enter your Password")
+    if username in usernames and password in passwords:
+      print ("Welcome!")
+    else:
+       print("Incorrect!")
+
+while True:
+  account_ready= input("Select : a:Sign Up  b:Login  c:Exit ")
+  if account_ready =="a":
+     register()
+  if account_ready=="b":
+     login()
+  if account_ready=="c":
+      break
